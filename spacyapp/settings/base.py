be7f72a8f9cc1930bba4159a28753d8d033df509
Matthias Schlögl <m.schloegl@gmail.com>
2018-01-31 15:01:56 +0100

    removed session uthentication in order to remove csrf test

--- spacyapp/settings/base.py
+++ spacyapp/settings/base.py
@@ -38,9 +38,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap3"
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES':
     ('rest_framework.permissions.AllowAny',),
