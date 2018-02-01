0ecc54c56c6f847a7dd0cc1d887086b4a165350b
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-02-01 15:44:39 +0100

    added a token counter property

--- enrich/custom_renderers.py
+++ enrich/custom_renderers.py
@@ -8,6 +8,7 @@ class DocToJsonRenderer(renderers.JSONRenderer):
         enriched = {}
         sents = [x for x in data.sents]
         result = []
+        counter = 0
         for x in sents:
             chunk = {}
             chunk['sent'] = "{}".format(x)
@@ -21,7 +22,9 @@ class DocToJsonRenderer(renderers.JSONRenderer):
                 parts['dep'] = y.dep_
                 parts['shape'] = y.shape_
                 parts['is_alpha'] = y.is_alpha
+                parts['token_id'] = counter
                 chunk['tokens'].append(parts)
+                counter += 1
             result.append(chunk)
         enriched['result'] = result
         res = super(DocToJsonRenderer, self).render(enriched, "application/json", renderer_context)
