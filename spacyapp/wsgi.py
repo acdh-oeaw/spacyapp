987b4a76045bf83624f2f5eff32fad993fd2dac9
Matthias Schlögl <m.schloegl@gmail.com>
2018-02-19 15:59:14 +0100

    wrong wsgi

--- spacyapp/wsgi.py
+++ spacyapp/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/var/www/html')
+sys.path.append('/var/www/html/myenv/lib/python3.5/site-packages')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spacyapp.settings")
 
