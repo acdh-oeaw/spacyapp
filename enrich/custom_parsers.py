c868e4b67904a99f7e44d6dccce78a87b9bd28ed
Matthias Schlögl <m.schloegl@gmail.com>
2018-07-16 15:49:24 +0200

    First version of agreement API

--- enrich/custom_parsers.py
+++ enrich/custom_parsers.py
@@ -1,18 +1,19 @@
-import spacy
-from spacy.tokens import Doc, Token
-from rest_framework.parsers import JSONParser
-
 import jmespath
+import spacy
+from rest_framework.parsers import JSONParser
+from spacy.tokens import Doc, Token
 
-spacy_lang_lst = {"german": "de_core_news_sm",
-                  "de": "de_core_news_sm",
-                  "deutsch": "de_core_news_sm",
-                  "ger": "de_core_news_sm"}
+spacy_lang_lst = {
+    "german": "de_core_news_sm",
+    "de": "de_core_news_sm",
+    "deutsch": "de_core_news_sm",
+    "ger": "de_core_news_sm"
+}
 
 spacy_pipeline = ['tagger', 'parser', 'ner']
 
 
-def process_tokenlist(nlp, tokenlist, enriched=None):
+def process_tokenlist(nlp, tokenlist, enriched=False):
     """ if enriched=True the created doc object runs through the nlp-processing-pipeline """
     json = {}
     json['tokenArray'] = tokenlist
@@ -32,20 +33,44 @@ def process_tokenlist(nlp, tokenlist, enriched=None):
 
 
 class JsonToDocParser(JSONParser):
-    """Parser parsing a Json into a spacy Doc element"""
+    """Parser parsing a Json into a spacy Doc element
+    Takes either a list of tokenArrays or one tokenArray, a language and a options parameter.
+    Returns either a list of docs or a single doc, a nlp element and the options
+    """
 
     media_type = "application/json+acdhlang"
 
     def parse(self, stream, media_type=None, parser_context=None):
-        json = super(JsonToDocParser, self).parse(stream, "application/json", parser_context)
+        json = super(
+            JsonToDocParser,
+            self,
+        ).parse(
+            stream,
+            "application/json",
+            parser_context,
+        )
         lang = json.get("language", "german")
         options = json.get("options", False)
         disable_pipeline = []
         if options:
             pipel = jmespath.search('outputproperties.pipeline', options)
             disable_pipeline = [x for x in spacy_pipeline if x not in pipel]
-        nlp = spacy.load(spacy_lang_lst[lang.lower()], disable=disable_pipeline)
-        doc = process_tokenlist(nlp, json['tokenArray'])
+        nlp = spacy.load(
+            spacy_lang_lst[lang.lower()],
+            disable=disable_pipeline,
+        )
+        token_array = json.get('tokenArray', None)
+        if token_array is None:
+            doc_list = json.get('docList', None)
+            if doc_list is not None:
+                doc = []
+                for token_array in doc_list:
+                    doc2 = process_tokenlist(nlp, token_array)
+                    doc.append(doc2)
+            else:
+                return None
+        else:
+            doc = process_tokenlist(nlp, json['tokenArray'])
         # ar_tok = [x['value'] for x in json['tokenArray']]
         # ar_wsp = [x.get('whitespace', True) for x in json['tokenArray']]
         # doc = Doc(nlp.vocab, words=ar_tok, spaces=ar_wsp)
