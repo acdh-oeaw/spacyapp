515f5791242793b8a883ad737430a02efe8561cd
Matthias Schlögl <m.schloegl@gmail.com>
2018-10-18 15:55:41 +0200

    changed the classes and the conversion processes notation

--- enrich/pipeline_processes/conversion.py
+++ enrich/pipeline_processes/conversion.py
@@ -1,10 +1,11 @@
 from enrich.tfc import Tcf
 from enrich.tei import TeiReader
 from enrich.custom_renderers import doc_to_tokenlist
-from .base import check_validity_payload 
+from enrich.custom_parsers import process_tokenlist
+#from .base import check_validity_payload 
 
 
-mapping_converters = {'from': {
+MAPPING_CONVERTERS_BAK = {'from': {
     'application/xml+tei': (TeiReader, 'create_tokenlist'),
     'spacyDoc': (doc_to_tokenlist,),
     'application/xml+tcf': (Tcf, 'create_tokenlist')
@@ -14,26 +15,56 @@ mapping_converters = {'from': {
     }
 }
 
+MAPPING_CONVERTERS = {'from': {
+    'application/xml+tei': (TeiReader, [('xml', 'original_xml')], 'create_tokenlist', [],),
+    'spacyDoc': (doc_to_tokenlist, [('doc', 'payload')]),
+    'application/xml+tcf': (Tcf, [('xml', 'original_xml')], 'create_tokenlist', [],)
+},
+    'to': {
+        'application/xml+tei': (TeiReader, 'process_tokenlist'),
+        'spacyDoc': (process_tokenlist, [('nlp', 'nlp'), ('tokenlist', '$data_json')])
+    }
+}
+
 
 class Converter:
 
-    def convert(self, to):
-        if len(mapping_converters['to'][to]) == 2:
-            self.data_converted = mapping_converters['to'][to][0](self.data_json)
-            self.data_converted = getattr(self.data_converted, mapping_converters['to'][to][1])()
+    def convert_bak(self, to):
+        if len(MAPPING_CONVERTERS['to'][to]) == 2:
+            self.data_converted = MAPPING_CONVERTERS['to'][to][0](self.data_json)
+            self.data_converted = getattr(self.data_converted, MAPPING_CONVERTERS['to'][to][1])()
         else:
-            self.data_converted = mapping_converters['to'][to][0](self.data_json)
+            self.data_converted = MAPPING_CONVERTERS['to'][to][0](self.data_json)
         return self.data_converted
 
-    def __init__(self, data_type=None, data=None):
-        if data_type not in mapping_converters['from'].keys():
+    def _convert_internal(self, l, t):
+        attr_dict = {}
+        to = MAPPING_CONVERTERS[l][t]
+        for d in to[1]:
+            if d[1].startswith('$'):
+                attr_dict[d[0]] = getattr(self, d[1][1:])
+            else:
+                attr_dict[d[0]] = getattr(self.original_process, d[1])
+        print(attr_dict)
+        data_converted = to[0](**attr_dict)
+        if len(to) > 2:
+            attr_dict = {}
+            for d in to[3]:
+                attr_dict[d[0]] = getattr(self.original_process, d[1])
+            data_converted = getattr(data_converted, to[2])(**attr_dict)
+        return data_converted
+
+    def convert(self, to):
+        self.data_converted = self._convert_internal('to', to)
+    
+    def __init__(self, data_type=None, data=None, original_process=None):
+        if data_type not in MAPPING_CONVERTERS['from'].keys() and data_type != 'application/json+acdhlang':
             raise ValueError('Data type specified is not supported by the converter.')
-        if not check_validity_payload(data_type, data):
-            raise ValueError('Payload has not the specified format.')
+        if original_process is None:
+            raise ValueError('Original process must be specified to get original files.')
+        else:
+            self.original_process = original_process
         if data_type == 'application/json+acdhlang':
             self.data_json = data
-        elif len(mapping_converters['from'][data_type]) == 2:
-            self.data_json = mapping_converters['from'][data_type][0](data)
-            self.data_json = getattr(self.data, mapping_converters[data_type[1]])()
         else:
-            self.data_json = mapping_converters['from'][data_type][0](data)
+            self.data_json = self._convert_internal('from', data_type)
--- enrich/test_pipeline_conversion.py
+++ enrich/test_pipeline_conversion.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 import spacy
 from enrich.pipeline_processes.conversion import Converter
+from enrich.pipeline_processes.base import SpacyProcess
+import json
 
 
 class ConversionTestCase(TestCase):
@@ -9,6 +11,11 @@ class ConversionTestCase(TestCase):
         nlp = spacy.load('de')
         txt = "Wien ist eine schöne Stadt"
         self.spacy_doc = nlp(txt)
+        with open('jsonschema/acdh_json_example.json') as f:
+            self.json_data = json.load(f)
 
     def test_spacy_to_json(self):
         d = Converter(data_type='spacyDoc', data=self.spacy_doc)
+
+    def test_spacy_to_json_new(self):
+        d = SpacyProcess(payload=self.json_data, mime='application/json+acdhlang')
