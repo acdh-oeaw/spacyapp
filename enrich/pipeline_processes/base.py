09bd9e5965ad38d29cf8f0cc77352c3171dfb042
Matthias Schlögl <m.schloegl@gmail.com>
2018-10-18 15:55:41 +0200

    changed the classes and the conversion processes notation

--- enrich/pipeline_processes/base.py
+++ enrich/pipeline_processes/base.py
@@ -4,10 +4,8 @@ from jsonschema.exceptions import ValidationError
 import json
 import spacy
 from .conversion import Converter
+from enrich.custom_parsers import SPACY_LANG_LST, SPACY_PIPELINE
 
-
-
-SPACY_PIPELINE = ['tagger', 'parser', 'ner']
 
 
 def check_validity_payload(kind, payload):
@@ -27,27 +25,40 @@ def check_validity_payload(kind, payload):
 
 
 class PipelineProcessBase:
+    """PipelineProcessBase: Baseclass for deriving NLP processes"""
     accepts = ["application/json+acdhlang"]
     returns = "application/json+acdhlang"
     payload = None
     function = False
     url = False
     headers = False
+    valid = False
     
     def convert_payload(self):
-
+        self.payload = Converter(data_type=self.mime, data=self.payload, original_process=self).convert(to=self.accepts[0])
+        self.mime = self.accepts[0]
+        self.check_validity()
 
     def check_validity(self):
+        """check_validity: checks if the payload is accepted by the function.
+           Starts vonvering process if needed.
+        """
         if self.mime is None:
             raise ValueError('You must specify a mime type of the payload.')
         if self.payload is None:
             raise ValueError('You cant call pipeline processes without specifying a payload.')
-        if self.mime not in self.accepts:
-            raise ValueError('Mime type not accepted by the process.')
         if not check_validity_payload(self.mime, self.payload):
             raise ValueError('Payload is not in the correct format')
+        if self.mime not in self.accepts:
+            self.convert_payload()
+        self.valid = True
 
     def __init__(self, payload=None, mime=None):
+        """__init__
+
+        :param payload: data for the process
+        :param mime: mime type of the payload data
+        """
         self.payload = payload
         self.mime = mime
         self.check_validity()
@@ -58,23 +69,28 @@ class SpacyProcess(PipelineProcessBase):
     returns = "spacyDoc"
 
     def process(self):
-        if self.options is not None:
-            if self.options['model']:
-                nlp = spacy.load(self.options['model'])
-            elif self.options['']
-                nlp = spacy.load('de')
-        if self.pipeline is None:
-            disable_pipeline = [x for x in SPACY_PIPELINE]
-        else:
-            disable_pipeline = [
-                x for x in SPACY_PIPELINE if x not in pipel
-            ]
-        if modell is None:
-            modell = SPACY_LANG_LST[lang.lower()]
-        else:
-            modell = os.path.join('~/media/pipeline_models', modell)
+       pass 
 
-    def __init__(self, payload=None, pipeline=None, options=None):
-        super().__init__(payload=payload)
+    def __init__(self, options=None, pipeline=None, **kwargs):
         self.pipeline = pipeline
         self.options = options
+        if self.options is not None:
+            if self.options['model']:
+                model = os.path.join('~/media/pipeline_models', self.options['model']) 
+            elif self.options['language']:
+                model = SPACY_LANG_LST[self.options['language'].lower()]
+        else:
+            model = 'de'
+        if self.pipeline is None:
+            disable_pipeline = []
+        else:
+            disable_pipeline = [
+                x for x in SPACY_PIPELINE if x not in self.pipeline
+            ]
+        self.nlp = spacy.load(
+            model,
+            disable=disable_pipeline,
+        )
+        super().__init__(**kwargs)
+        if not self.valid:
+            raise ValueError('Something went wrong in the data conversion. Data is not valid.')
