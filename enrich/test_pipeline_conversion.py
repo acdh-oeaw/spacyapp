207637d7980e7dba483986b6245d2880b4bd54f4
Matthias Schlögl <m.schloegl@gmail.com>
2018-10-19 11:50:34 +0200

    spacyprocess working

--- enrich/test_pipeline_conversion.py
+++ enrich/test_pipeline_conversion.py
@@ -1,21 +1,42 @@
-from django.test import TestCase
-import spacy
-from enrich.pipeline_processes.conversion import Converter
-from enrich.pipeline_processes.base import SpacyProcess
 import json
+
+import spacy
+from django.test import TestCase
+
+from enrich.pipeline_processes.base import SpacyProcess
+from enrich.pipeline_processes.conversion import Converter
 
 
 class ConversionTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        nlp = spacy.load("de")
+        cls.txt = "Wien ist eine schöne Stadt"
+        cls.spacy_doc = nlp(cls.txt)
+        with open("jsonschema/acdh_json_example.json") as f:
+            cls.json_data = json.load(f)
 
-    def setUp(self):
-        nlp = spacy.load('de')
-        txt = "Wien ist eine schöne Stadt"
-        self.spacy_doc = nlp(txt)
-        with open('jsonschema/acdh_json_example.json') as f:
-            self.json_data = json.load(f)
+    def test_json_to_spacy(self):
+        d = SpacyProcess(payload=self.json_data, mime="application/json+acdhlang")
+        res = d.process()
+        print([x for x in res.ents])
+        ents = [str(x) for x in res.ents]
+        self.assertIn("Wien", ents)
 
-#    def test_spacy_to_json(self):
-#       d = Converter(data_type='spacyDoc', data=self.spacy_doc)
+    def test_plaintext_to_spacy(self):
+        d = SpacyProcess(payload=self.txt, mime="text/plain")
+        res = d.process()
+        print([x for x in res.ents])
+        ents = [str(x) for x in res.ents]
+        self.assertIn("Wien", ents)
 
-    def test_spacy_to_json_new(self):
-        d = SpacyProcess(payload=self.json_data, mime='application/json+acdhlang')
+    def test_json_to_spacy_without_ner(self):
+        d = SpacyProcess(
+            payload=self.json_data,
+            mime="application/json+acdhlang",
+            pipeline=["tagger", "parser"],
+        )
+        res = d.process()
+        print([x for x in res.ents])
+        ents = [str(x) for x in res.ents]
+        self.assertNotIn("Wien", ents)
