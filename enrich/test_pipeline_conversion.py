88ec57eea83dc05bcd87e4c5bd62017fbeb4a506
Matthias Schlögl <m.schloegl@gmail.com>
2018-11-07 10:02:55 +0100

    added a xtx process and a basic test

--- enrich/test_pipeline_conversion.py
+++ enrich/test_pipeline_conversion.py
@@ -3,7 +3,7 @@ import json
 import spacy
 from django.test import TestCase
 
-from enrich.pipeline_processes.base import SpacyProcess
+from enrich.pipeline_processes.base import SpacyProcess, XtxProcess
 from enrich.pipeline_processes.conversion import Converter
 
 
@@ -15,6 +15,8 @@ class ConversionTestCase(TestCase):
         cls.spacy_doc = nlp(cls.txt)
         with open("jsonschema/acdh_json_example.json") as f:
             cls.json_data = json.load(f)
+        with open("data/editions/abschrift-artikel-allgemeine-kirchenzeitung-1850-a3-xxi-d91.xml", "r") as f:
+            cls.tei_data = f.read()
 
     def test_json_to_spacy(self):
         d = SpacyProcess(payload=self.json_data, mime="application/json+acdhlang")
@@ -40,3 +42,8 @@ class ConversionTestCase(TestCase):
         print([x for x in res.ents])
         ents = [str(x) for x in res.ents]
         self.assertNotIn("Wien", ents)
+
+    def test_xtx_process(self):
+        d = XtxProcess(payload=self.tei_data, mime="application/xml+tei")
+        res = d.process()
+        print(res)
