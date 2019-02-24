3b223c0aafc3df3972a18f3f68a8e76ef6de7acc
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-02-24 18:36:01 +0100

    added helper function to create word embeddings and to evaluate models

--- enrich/spacy_utils/train.py
+++ enrich/spacy_utils/train.py
@@ -1,40 +1,24 @@
-#
-# def train(lang='de_core_news_sm', output_dir=None, n_iter=1, TRAIN_DATA=TRAIN_DATA[:4], TEST_DATA=TEST_DATA):
-#     """Create a new model, set up the pipeline and train the tagger. In order to
-#     train the tagger with a custom tag map, we're creating a new Language
-#     instance with a custom vocab.
-#     """
-#     nlp = spacy.load(lang)
-#     # add the tagger to the pipeline
-#     # nlp.create_pipe works for built-ins that are registered with spaCy
-#     tagger = nlp.get_pipe('tagger')
-#     # Add the tags. This needs to be done before you start training.
-# #     for tag, values in TAG_MAP.items():
-# #         tagger.add_label(tag, values)
-#
-#     optimizer = nlp.begin_training()
-#     for i in range(n_iter):
-#         random.shuffle(TRAIN_DATA)
-#         losses = {}
-#         for text, annotations in TRAIN_DATA:
-#             nlp.update([text], [{'words': annotations[0], 'tags': annotations[1]}], sgd=optimizer, losses=losses)
-#         print(losses)
-#
-#     # test the trained model
-#     test_text = TEST_DATA
-#     doc = nlp(test_text)
-#     print('Tags', [(t.text, t.tag_, t.pos_) for t in doc])
-#
-#     # save model to output directory
-#     if output_dir is not None:
-#         output_dir = Path(output_dir)
-#         if not output_dir.exists():
-#             output_dir.mkdir()
-#         nlp.to_disk(output_dir)
-#         print("Saved model to", output_dir)
-#
-#         # test the save model
-#         print("Loading from", output_dir)
-#         nlp2 = spacy.load(output_dir)
-#         doc = nlp2(test_text)
-#         print('Tags', [(t.text, t.tag_, t.pos_) for t in doc])
+import spacy
+from spacy.gold import GoldParse
+from spacy.scorer import Scorer
+
+
+def evaluate(ner_model, examples):
+    scorer = Scorer()
+    for x in examples:
+        doc_gold_text = ner_model.make_doc(x[0])
+        gold = GoldParse(doc_gold_text, entities=x[1]['entities'])
+        pred_value = ner_model(x[0])
+        scorer.score(pred_value, gold)
+    return scorer.scores
+
+
+def compare_models(models, examples):
+    for x in models:
+        ner_model = spacy.load(x)
+        results = evaluate(ner_model, examples)
+        print(
+            x, "p: {}; f: {}; r: {}".format(
+                results['ents_p'], results['ents_f'], results['ents_r']
+            )
+        )
